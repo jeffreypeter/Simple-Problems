16e851026607d633bfcf8325293c58afa445d204
jeffravi <jeffravi@iu.edu>
2016-11-09 08:53:42 -0500

    Manasa and Stones Init

---
+++ Python/Manasa and Stones/Manasa and stones.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+tc = int(raw_input())
+ip = list()
+for count in range(tc):
+    ip.append((raw_input(),raw_input(),raw_input()))
+print ip
