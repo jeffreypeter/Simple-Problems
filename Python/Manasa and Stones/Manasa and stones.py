b0c464c58ae85a50c11241a5c7c5fecd0794f20f
jeffravi <jeffravi@iu.edu>
2016-11-09 10:24:41 -0500

    Manasa and Stones Completed

--- Python/Manasa and Stones/Manasa and stones.py
+++ Python/Manasa and Stones/Manasa and stones.py
@@ -1,6 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-tc = int(raw_input())
-ip = list()
-for count in range(tc):
-    ip.append((raw_input(),raw_input(),raw_input()))
-print ip
+import itertools as it
+for count in range(int(raw_input())):
+    (n,a,b) = (raw_input(),int(raw_input()),int(raw_input()))
+    comb = list(it.combinations_with_replacement((a,b),int(n)))
+    dist =  sorted(set([(sum(x)) for x in comb]))
+    print " ".join(map(str, dist))
